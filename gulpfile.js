af31f2d83c3545ce2364053a09450a9e2037843b
Dylan <dylansamuelboyle@gmail.com>
2018-05-22 00:16:13 -0400

    Adding browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,16 @@ var gulp = require('gulp');
 var print = require('gulp-print').default;
 var less = require('gulp-less');
 var babel = require('gulp-babel');
+var sync = require('browser-sync');
+var reload = sync.reload;
+
+gulp.task('sync', function() {
+    sync({
+        server: {
+            baseDir: "./app"
+        }
+    });
+});
 
 gulp.task('less', function () {
     return gulp.src('app/styles.less')
@@ -17,4 +27,12 @@ gulp.task('js', function() {
         presets: ['es2015']
     }))
     .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('reload', function() {
+    sync.reload();
+});
+
+gulp.task('default', ['sync'], function() {
+    gulp.watch("/app/index.html", ['reload']);
 });
